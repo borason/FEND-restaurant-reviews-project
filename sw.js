e2ef1ca7d57bd9b71e2d593ed8d119fa21be6ad8
Bora Son <btson310@gmail.com>
2018-09-30 14:36:59 -0600

    save before logging out september 30

--- sw.js
+++ sw.js
@@ -1,42 +1,43 @@
 const cacheName = 'fend-restaurant-v1';
-// const cacheAssets = [
-//     '/',
-//     '/index.html',
-//     '/restaurant.html',
-//     '/css/styles.css',
-//     '/js/dbhelper.js',
-//     '/js/main.js',
-//     '/js/restaurant_info.js',
-//     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
-//     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
-//     '/data/restaurants.json',
-//     '/img/1.jpg',
-//     '/img/2.jpg',
-//     '/img/3.jpg',
-//     '/img/4.jpg',
-//     '/img/5.jpg',
-//     '/img/6.jpg',
-//     '/img/7.jpg',
-//     '/img/8.jpg',
-//     '/img/9.jpg',
-//     '/img/10.jpg'
-// ];
+
+const cacheAssets = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
+];
 
 // Install Event
-// self.addEventListener('install', event => {
-//   console.log('Service Worker Installed');
-//   // Waits Until It Opens Cache
-//   event.waitUntil(
-//     caches
-//       .open(cacheName)
-//       // Caches Assets
-//       .then(cache => {
-//         console.log('Service Worker Caching Files');
-//         cache.addAll(cacheAssets);
-//       })
-//       .then(() => self.skipWaiting())
-//   );
-// });
+self.addEventListener('install', event => {
+  console.log('Service Worker Installed');
+  // Waits Until It Opens Cache
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      // Caches Assets
+      .then(cache => {
+        console.log('Service Worker Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
 
 // Activate Event
 self.addEventListener('activate', event => {
